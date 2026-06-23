f20a1b5ce4e172b481bb4c57e6048ce9fd7832db
Aayush Shukla <aayush.shukla23@gmail.com>
2026-06-23 13:18:52 +0530

    Completed implementation

--- controllers/user.go
+++ controllers/user.go
@@ -1,1 +1,76 @@
 package controllers
+
+import(
+	"fmt"
+	"encoding/json"
+	"github.com/julienschmidt/httprouter"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	"net/http"	
+	"github.com/ars-shukla23/mongo-golang/models"
+)
+type UserController struct{
+	session *mgo.Session
+}
+
+func NewUserController(s *mgo.Session) *UserController{
+	return &UserController{s}
+}
+
+func (uc UserController) GetUser(w http.ResponseWriter, r *http.Request, p httprouter.Params){
+	Id:=p.ByName("id")
+
+	if !bson.IsObjectIdHex(Id){
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	oid:=bson.ObjectIdHex(Id)
+	u:=models.User{}
+	err:=uc.session.DB("mongo-golang").C("users").FindId(oid).One(&u)
+	if err!=nil{
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	uj,err:=json.Marshal(u)
+	if err!=nil{
+		fmt.Println(err)
+	}
+	w.Header().Set("Content-Type","application/json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w,"%s\n",uj) // or w.Write(uj)
+}
+
+func (uc UserController) CreateUser(w http.ResponseWriter, r *http.Request, _ httprouter.Params){
+	u:=models.User{}
+	err:=json.NewDecoder(r.Body).Decode(&u)
+	if err!=nil{
+		fmt.Println(err)
+	}
+	u.Id=bson.NewObjectId()
+	uc.session.DB("mongo-golang").C("users").Insert(u)
+
+	uj,err:=json.Marshal(u)
+	if err!=nil{
+		fmt.Println(err)
+	}
+	w.Header().Set("Content-Type","application/json")
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprintf(w,"%s\n",uj)
+}
+
+func (uc UserController) DeleteUser(w http.ResponseWriter, r *http.Request, p httprouter.Params){
+	Id:=p.ByName("id")
+	if !bson.IsObjectIdHex(Id){
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	oid:=bson.ObjectIdHex(Id)
+	err:=uc.session.DB("mongo-golang").C("users").RemoveId(oid)
+	if err!=nil{
+		w.WriteHeader(http.StatusNotFound)
+	}
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w,"Deleted user %s\n",oid)
+	
+	
+}
--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
 import(
-
+   "github.com/julienschmidt/httprouter"
+   "gopkg.in/mgo.v2"
+   "net/http"
+   "github.com/ars-shukla23/mongo-golang/controllers"
 )
-
+ 
 func main(){
-	
+  r:=httprouter.New()
+  uc:=controllers.NewUserController(getSession())
+  r.GET("/user/:id",uc.GetUser)
+  r.POST("/user",uc.CreateUser)
+  r.DELETE("/user/:id",uc.DeleteUser)
+  http.ListenAndServe("localhost:9000",r)
+ 
+} 
+
+func getSession() *mgo.Session{
+   session,err:=mgo.Dial("mongodb://localhost:27017")
+   if err!=nil{
+	  panic(err)
+   }
+   return session
 }
